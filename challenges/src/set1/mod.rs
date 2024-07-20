e2e96531e48201635b6f0e732a4928c867337c75
Andrea Francesco Iuorio <andrea@iuorio.eu>
2024-07-20 20:33:17 +0200

    set set1

--- challenges/src/set1/mod.rs
+++ challenges/src/set1/mod.rs
@@ -17,12 +17,12 @@ use crate::set1::s1c7::s1c7;
 use crate::set1::s1c8::s1c8;
 
 pub fn run_challenges(){
-    //s1c1();
-    //s1c2();
-    //s1c3();
-    //s1c4();
-    //s1c5();
-    //s1c6();
-    //s1c7();
+    s1c1();
+    s1c2();
+    s1c3();
+    s1c4();
+    s1c5();
+    s1c6();
+    s1c7();
     s1c8();
 }
